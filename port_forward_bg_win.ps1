9b938762e22b74b54c68bcf6ad789de28c6845b3
Mr. Intelligence Inc <sainathkishore@mrintelligenceinc.com>
2025-12-04 23:00:55 +0530

    Update port_forward_bg_win.ps1

--- port_forward_bg_win.ps1
+++ port_forward_bg_win.ps1
@@ -1,26 +1,26 @@
-# Stop previous port-forward background jobs
-Get-Job | Remove-Job -Force
+# # Stop previous port-forward background jobs
+# Get-Job | Remove-Job -Force
 
-Write-Host "`nStarting Port-Forward for Role-Service (4101) ..."
-Start-Job -ScriptBlock {
-    kubectl port-forward deployment/role-service 4101:4101 -n petlinks-dev
-}
+# Write-Host "`nStarting Port-Forward for Role-Service (4101) ..."
+# Start-Job -ScriptBlock {
+#     kubectl port-forward deployment/role-service 4101:4101 -n petlinks-dev
+# }
 
-Write-Host "Starting Port-Forward for Gateway (4013) ..."
-Start-Sleep -Seconds 2   # IMPORTANT DELAY
+# Write-Host "Starting Port-Forward for Gateway (4013) ..."
+# Start-Sleep -Seconds 2   # IMPORTANT DELAY
 
-Start-Job -ScriptBlock {
-    kubectl port-forward deployment/gateway 4013:4013 -n petlinks-dev
-}
+# Start-Job -ScriptBlock {
+#     kubectl port-forward deployment/gateway 4013:4013 -n petlinks-dev
+# }
 
-# Wait a few seconds to ensure startup
-Start-Sleep -Seconds 3
+# # Wait a few seconds to ensure startup
+# Start-Sleep -Seconds 3
 
-Write-Host "`nChecking active ports:"
-netstat -ano | findstr ":4013"
-netstat -ano | findstr ":4101"
+# Write-Host "`nChecking active ports:"
+# netstat -ano | findstr ":4013"
+# netstat -ano | findstr ":4101"
 
-Write-Host "`nPort forwarding now running in background"
+# Write-Host "`nPort forwarding now running in background"
 
 # TRUNCATE TABLE
 #   role_menu_permissions,
@@ -39,3 +39,11 @@ Write-Host "`nPort forwarding now running in background"
 # helm upgrade --install role-service ../services/core/thepetlink-role-service/helm/role-service -n petlinks-dev & kubectl delete pod -l app=role-service -n petlinks-dev & kubectl logs -f deployment/role-service -n petlinks-dev
 # docker build --no-cache -t gateway:latest .
 # helm upgrade --install gateway ../services/gateway/helm/gateway -n petlinks-dev & kubectl delete pod -l app=gateway -n petlinks-dev & kubectl logs -f deployment/gateway -n petlinks-dev
+
+Get-Job | Remove-Job -Force;
+Start-Job { kubectl port-forward deployment/role-service 4101:4101 -n petlinks-dev };
+Start-Job { kubectl port-forward deployment/gateway 4013:4013 -n petlinks-dev };
+Start-Job { kubectl port-forward pod/postgres-postgresql-0 5432:5432 -n petlinks-dev }
+netstat -ano | findstr 4101;
+netstat -ano | findstr 4013;
+netstat -ano | findstr 5432;
